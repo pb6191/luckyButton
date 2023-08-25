f24c8f52eaaebaae36a72e1f1a627a0a38c95b09
Puneet Bhargava <pbhargava936@gmail.com>
2023-08-25 11:17:53 -0400

    add comments

--- src/analysis.py
+++ src/analysis.py
@@ -39,22 +39,24 @@ SMALL_SIZE = 18
 MEDIUM_SIZE = 20
 BIGGER_SIZE = 22
 
-plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 #%%
 
+# read survey data
 df1 = pd.read_csv("../data/raw/luckybutton_June 25, 2023_09.53_numeric.csv")
 
-df = df1.drop(labels=[0,1], axis=0)
+df = df1.drop(labels=[0, 1], axis=0)
 
 #%%
 
+# hack to change textual numbers to numeric
 df.to_csv("../data/clean/clean_data.csv", index=False)
 df = pd.read_csv("../data/clean/clean_data.csv")
 df.dtypes
@@ -69,80 +71,99 @@ df["hover"].mean()
 df[(df.condition == "deterministic")]["hover"].mean()
 df[(df.condition == "probabilistic")]["hover"].mean()
 
-ttest_rel(df['freq'], df['freq2'])
-ttest_rel(df['freq2'], df['hover'])
 
-ttest_ind(df[(df.condition == "deterministic")]['hover'], df[(df.condition == "probabilistic")]['hover'])
+# t-test to check differences between group means
+ttest_rel(df["freq"], df["freq2"])
+ttest_rel(df["freq2"], df["hover"])
 
-
+ttest_ind(
+    df[(df.condition == "deterministic")]["hover"],
+    df[(df.condition == "probabilistic")]["hover"],
+)
 
 
 #%%
 
 
-
+# create the plot for different conditions
 
 fig, ax = plt.subplots(figsize=(15, 8))
 
 # Create a list of colors for the boxplots based on the number of features you have
-boxplots_colors = ['yellowgreen', 'olivedrab', 'yellowgreen']
+boxplots_colors = ["yellowgreen", "olivedrab", "yellowgreen"]
 
 dflist = df[["freq", "freq2", "hover"]].T.values.tolist()
 
 # Boxplot data
-bp = ax.boxplot(dflist, patch_artist = True, vert = False)
+bp = ax.boxplot(dflist, patch_artist=True, vert=False)
 
 # Change to the desired color and add transparency
-for patch, color in zip(bp['boxes'], boxplots_colors):
+for patch, color in zip(bp["boxes"], boxplots_colors):
     patch.set_facecolor(color)
     patch.set_alpha(0.4)
 
 # Create a list of colors for the violin plots based on the number of features you have
-violin_colors = ['thistle', 'orchid', 'thistle']
+violin_colors = ["thistle", "orchid", "thistle"]
 
 # Violinplot data
-vp = ax.violinplot(dflist, points=500, 
-               showmeans=False, showextrema=False, showmedians=False, vert=False)
+vp = ax.violinplot(
+    dflist,
+    points=500,
+    showmeans=False,
+    showextrema=False,
+    showmedians=False,
+    vert=False,
+)
 
-for idx, b in enumerate(vp['bodies']):
+for idx, b in enumerate(vp["bodies"]):
     # Get the center of the plot
     m = np.mean(b.get_paths()[0].vertices[:, 0])
     # Modify it so we only see the upper half of the violin plot
-    b.get_paths()[0].vertices[:, 1] = np.clip(b.get_paths()[0].vertices[:, 1], idx+1, idx+2)
+    b.get_paths()[0].vertices[:, 1] = np.clip(
+        b.get_paths()[0].vertices[:, 1], idx + 1, idx + 2
+    )
     # Change to the desired color
     b.set_color(violin_colors[idx])
 
 # Create a list of colors for the scatter plots based on the number of features you have
-scatter_colors = ['tomato', 'darksalmon', 'tomato']
+scatter_colors = ["tomato", "darksalmon", "tomato"]
 
 # Scatterplot data
 for idx, features in enumerate(dflist):
     # Add jitter effect so the features do not overlap on the y-axis
-    y = np.full(len(features), idx + .8)
+    y = np.full(len(features), idx + 0.8)
     idxs = np.arange(len(y))
     out = y.astype(float)
-    out.flat[idxs] += np.random.uniform(low=-.05, high=.05, size=len(idxs))
+    out.flat[idxs] += np.random.uniform(low=-0.05, high=0.05, size=len(idxs))
     y = out
-    plt.scatter(features, y, s=.3, c=scatter_colors[idx])
+    plt.scatter(features, y, s=0.3, c=scatter_colors[idx])
 
-plt.yticks(np.arange(1,4,1), ['at baseline', 'after learning\nwhat it does', 'if there was\na preview\nupon hovering'])  # Set text labels.
-plt.xlabel('')
+plt.yticks(
+    np.arange(1, 4, 1),
+    [
+        "at baseline",
+        "after learning\nwhat it does",
+        "if there was\na preview\nupon hovering",
+    ],
+)  # Set text labels.
+plt.xlabel("")
 plt.xlim([0, 8])
 ax.set_xticks([1, 2, 3, 4, 5, 6, 7])
-ax.set_xticklabels(["Never", "Very\nrarely", "Rarely", "Occasionally", "Frequently", "Very\nfrequently", "Always"])
+ax.set_xticklabels(
+    [
+        "Never",
+        "Very\nrarely",
+        "Rarely",
+        "Occasionally",
+        "Frequently",
+        "Very\nfrequently",
+        "Always",
+    ]
+)
 plt.title("Frequency of utilizing the 'I am feeling lucky' button")
 fig.tight_layout()
 fig.savefig("../figures/freq.png")
 plt.show()
 
 
-
-
-
-
-
-
 #%%
-
-
-
